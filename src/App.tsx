f6be39f19ccf163dba08ef52482b0a8d1861a91e
Sorna03 <112056145+Sorna03@users.noreply.github.com>
2024-07-15 19:55:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 function App() {
+  let items = ["Java", "Python", "C", "C++"];
+  const handleSelectItem=(item:string)=>{
+    console.log(item)
+  }
   const [alertVisible,setAlertVisible]=useState(false);
   return (
     <div>
+      <ListGroup items={items} heading="Select a Course" onSelectItem={handleSelectItem}/>
       {alertVisible&&<Alert onClose={()=>setAlertVisible(false)}>Enter all Details</Alert>}
-      <Button color="success"onClick={()=>setAlertVisible(true)}>Login</Button>
+      <Button color="success"onClick={()=>setAlertVisible(true)}>Register</Button>
     </div>
   );
 }
